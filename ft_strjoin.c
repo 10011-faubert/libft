620e37262641faedd75b64da5d9ceb7be5eaf83e
François Aubert <francoisaubert.pro@gmail.com>
2020-03-16 18:10:26 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,17 +18,19 @@ char				*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char	*str;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
-	if (!(str = ft_calloc(sizeof(char), (ft_strlen(s1) + ft_strlen(s2) + 1))))
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
 	i = 0;
-	j = 0;
 	while (s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
+	j = 0;
 	while (s2[j])
 	{
 		str[i + j] = s2[j];
